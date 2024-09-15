cd84b607abb2a17e6463f8c951aa45fb0f5d6578
CodeAtom1 <gourav.w82@gmail.com>
2024-09-15 22:35:04 +0530

    User List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchForm from './components/SearchForm';
+import UserList from './components/UserList';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <SearchForm />
+        <UserList />
       </header>
     </div>
   );
---
+++ src/components/UserList.js
@@ -0,0 +1,34 @@
+import React, {useState, useMemo} from "react";
+
+const users = [
+    { id: 1, name: 'John Doe', age: 25},
+    { id: 2, name: 'Bob Marlay', age:22},
+    { id: 3, name: 'Ken Benlay', age:32},
+    { id: 4, name: 'Rony Stephen', age: 51}
+    //Imagine this has milions of records
+  ];
+
+const UserList = () =>{
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filtredUsers = useMemo(() => {
+        console.log("Filtering users...");
+        return users.filter((user) => user.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    },[searchTerm]);
+
+    return (
+    <div>
+        <input type="text" placeholder="Search users" value={searchTerm} 
+        onChange={(e) => setSearchTerm(e.target.value)} />
+        <ul>
+            {
+                filtredUsers.map((user) =>
+                (<li key={user.id}>
+                    {user.name} - Age {user.age}
+                </li>))
+            }
+        </ul>
+    </div>)
+}
+
+export default UserList;
