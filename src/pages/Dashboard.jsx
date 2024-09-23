d2f3881d4526242020b9d1f663b0fb61a21f74a0
Gourav Wadhawan <Gourav.w82@gmail.com>
2024-09-23 15:07:57 +0530

    added routing to home page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,23 @@
 import Content from "../components/Content";
 import SideBar from "../components/SideBar";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import UserList from "../components/UserList";
+import { TopNavBar } from "../components/TopNavBar";
 export const Dashboard = () => {
 
     return (
         <>
-        <div className="container-fluid position-relative d-flex p-0">
-            <SideBar/>
-            <Content />
-        </div>
+        <Router>
+            <div className="container-fluid position-relative d-flex p-0">
+                <SideBar/>
+                <TopNavBar>
+                    <Routes>
+                        <Route path="/users" element={<UserList />} />
+                        <Route path="/dashboard" element={<Content />} />
+                    </Routes>
+                </TopNavBar>
+            </div>
+        </Router>
           <script src="js/main.js"></script>
         </>
     );
